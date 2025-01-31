2c6533e0a98499ec1e207948735d80707c9349c3
NymphaPamba <praisepamba@gmail.com>
2025-01-31 06:19:02 +0300

    initial commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';  // Import your pages
+import Home from './pages/Home';  
 import CreateRecipe from './pages/CreateRecipe';
 import RecipeList from './pages/RecipeList';
-import CreateCategory from './pages/CreateCategory.jsx';  // Import CreateCategory page
-import EditRecipes from './pages/EditRecipe.jsx';      // Import EditCategory page
-import Navbar from './components/Header'; // Import your Navbar
+import CreateCategory from './pages/CreateCategory.jsx';  
+import EditRecipes from './pages/EditRecipe.jsx';      
+import Navbar from './components/Header'; 
 
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Include your navbar */}
+      <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-recipe" element={<CreateRecipe />} />
         <Route path="/recipes" element={<RecipeList />} />
-        <Route path="/create-category" element={<CreateCategory />} /> {/* Define route for create category */}
-        <Route path="/edit:/id" element={<EditRecipes />} />       {/* Define route for edit category */}
+        <Route path="/create-category" element={<CreateCategory />} /> 
+        <Route path="/edit:/id" element={<EditRecipes />} />       
       </Routes>
     </Router>
   );
