379e270edfc995ecb1e21b4ec0c25835b5f95dea
NymphaPamba <praisepamba@gmail.com>
2025-01-31 06:11:52 +0300

    initial commits

--- backend/run.py
+++ backend/run.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-from app.routes import routes  # Ensure the correct import
+from app.routes import routes  
 
 app = Flask(__name__)
 
-# Enable CORS for the entire app
 CORS(app)
 
-# Register the routes blueprint
-app.register_blueprint(routes)  # This is where you register the routes blueprint
+app.register_blueprint(routes)  
 
 if __name__ == '__main__':
     app.run(debug=True)
