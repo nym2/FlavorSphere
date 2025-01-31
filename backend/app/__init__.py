a8397baa806a576f9070d46e946316796bdc8b2f
NymphaPamba <praisepamba@gmail.com>
2025-01-31 07:12:49 +0300

    initial commits

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_migrate import Migrate  
 from .models import db
 from .routes import routes
 from flask_cors import CORS
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="frontend/build", static_url_path="")  
     
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,9 +19,18 @@ def create_app():
 
     app.register_blueprint(routes)
 
+    
+    @app.route("/", defaults={"path": ""})
+    @app.route("/<path:path>")
+    def serve_react_app(path):
+        
+        if path and os.path.exists(os.path.join(app.static_folder, path)):
+            return send_from_directory(app.static_folder, path)
+        
+        return send_from_directory(app.static_folder, "index.html")
+
     return app
 
 if __name__ == '__main__':
     app = create_app()
-    
     app.run(debug=True, host='0.0.0.0')
